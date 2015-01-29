954c4dcf6def4fc2613db90bd691b1499aec5027
lu-c <tangerinesauce@gmail.com>
2015-01-29 13:12:13 -0800

    -added radial chart card to examples

--- app/helpers/sample_funtime_deck_helper.rb
+++ app/helpers/sample_funtime_deck_helper.rb
@@ -2,8 +2,12 @@ module SampleFuntimeDeckHelper
   def render_sample_funtime_deck
     deck_config = {id: 'party', controls: ['refresh', 'search']}
     people_visualizations = [
-        {id: 'friends_radial_chart', type: 'radial', title: 'Friends', data_source: 'collect_friends_data', description: 'friend desc'},
-        {id: 'enemies_radial_chart', type: 'radial', title: 'Enemies', data_source: 'collect_enemies_data', description: 'enemy desc'}
+        {id: 'friends_radial_chart', type: 'radial', title: 'Friends', data_source: 'collect_friends_data', description: 'friend desc', showLegend: false, theme: 'green', style: 'concentric'},
+        {id: 'enemies_radial_chart', type: 'radial', title: 'Enemies', data_source: 'collect_enemies_data', description: 'enemy desc'},
+        {id: 'time_radial_chart', type: 'radial', title: 'Time', data_source: 'collect_time_data', sort: true},
+    ]
+    ice_cream_visualizations = [
+        {id: 'icecream_radial_chart', type: 'radial', title: 'Ice Cream', data_source: 'collect_icecream_data', description: 'Flavors', style: 'cumulative'},
     ]
 
     render_deckster_deck deck_config, [
@@ -13,26 +17,47 @@ module SampleFuntimeDeckHelper
         {card: :pinata, load: :async, title: "Pinata", row: 1, col: 2, sizex: 1, sizey: 1},
         {card: :beer, load: :async, title: "Beer", row: 1, col: 2, sizex: 1, sizey: 1, display: 'none'}, #example of hidden card
         {card: :music, load: :async, title: "Music", row: 1, col: 2, sizex: 1, sizey: 1, display: 'none'}, #example of hidden card
-        {card: :people, load: :async, title: "People", row: 1, col: 1, sizex: 2, sizey: 1, visualizations: people_visualizations}
+        {card: :people, load: :async, title: "People", row: 1, col: 1, sizex: 2, sizey: 1, visualizations: people_visualizations},
+        {card: :icecream, load: :async, title: "Ice Cream", row: 1, col: 1, sizex: 1, sizey: 1, visualizations: ice_cream_visualizations}
+    ]
+  end
+
+  def collect_icecream_data
+    [
+      { percent: 20, description: 'Vanilla'},
+      { percent: 50, description: 'Chocolate'},
+      { percent: 30, description: 'Strawberry'}
+    ]
+  end
+
+  def render_icecream_summary_card
+    ""
+  end
+
+  def render_icecream_detail_card
+    ""
+  end
+
+  def collect_time_data
+    [
+        { percent: 60, description: 'Hours' },
+        { percent: 100, description: 'Weeks' },
+        { percent: 80, description: 'Days' },
+        { percent: 40, description: 'Minutes' },
+        { percent: 20, description: 'Seconds' }
     ]
   end
 
   def collect_friends_data
     [
-      {
-            percent: 0.15,
-        description: 'EoE'
-      },
-      {
-        percent: 0.60,
-        description: 'friends'
-      }
+        { percent: 60, description: 'friends' },
+        { percent: 35, description: 'EoE' }
     ]
   end
 
   def collect_enemies_data
     {
-        percent: 0.25,
+        percent: 25,
     }
   end
 
